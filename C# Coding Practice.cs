50fff64c37c22c6e93bbf7fffc9b010933c055ee
Jordan Rhinerson <jordan.rhinerson@gmail.com>
2025-04-06 21:18:26 -0400

    Array of directions

--- C# Coding Practice.cs
+++ C# Coding Practice.cs
@@ -2,7 +2,7 @@ using System;
 public static class CodeWarsPractice
 {
 // 04-06-25
-// Given a string remove all vowels and return the string without vowels
+  // Given a string remove all vowels and return the string without vowels
     public static string Disemvowel(string str)
     {
       string newStr = "";
@@ -17,5 +17,31 @@ public static class CodeWarsPractice
       return newStr;
     }
 
-    
+  // Given an array of strings indicating direction of 1 minute each determine if the given walk will last 10 minutes and return you back to where you started
+    public static bool IsValidWalk(string[] walk)
+    {
+      if (walk.Length != 10) 
+        return false;
+      var vertical = 0;
+      var horizontal = 0;
+      foreach (string direction in walk)
+      {
+          switch (direction)
+          {
+              case "n":
+                  vertical++;
+                  break;
+              case "s":
+                  vertical--;
+                  break;
+              case "e":
+                  horizontal++;
+                  break;
+              case "w":
+                  horizontal--;
+                  break;
+          }
+      }
+      return (vertical == 0 && horizontal == 0);
+    }
 }
